74876e27c52b1d730a0634b2be465e2c90f5ea3e
Nicholas Wildason-Ferreri <nickferreri@Nicks-iMac-2017.local>
2023-08-30 14:58:13 -0400

    Add error handling

--- JPGtoPNGconverter.py
+++ JPGtoPNGconverter.py
@@ -6,18 +6,22 @@ import sys
 import os
 from PIL import Image
 
-# Get source folder and output folder
-location = str(sys.argv[1])
-new_folder = str(sys.argv[2])
+# Error handling
+try:
+    # Get source folder and output folder
+    location = str(sys.argv[1])
+    new_folder = str(sys.argv[2])
+except IndexError:
+    print("ERROR: Please enter input and output folders.")
+else:
+    # Create output folder if it doesn't exist
+    if not os.path.exists(new_folder):
+        os.makedirs(new_folder)
 
-# Create output folder if it doesn't exist
-if not os.path.exists(new_folder):
-    os.makedirs(new_folder)
-
-# Convert .jpg files in source folder to .png in output folder
-for file in os.listdir(location):
-    if '.jpg' in file:
-        img = Image.open(location + file)
-        name = os.path.splitext(file)[0]
-        img.save(new_folder + name + '.png', 'png')
-        print("All done!")
+    # Convert .jpg files in source folder to .png in output folder
+    for file in os.listdir(location):
+        if '.jpg' in file:
+            img = Image.open(location + file)
+            name = os.path.splitext(file)[0]
+            img.save(new_folder + name + '.png', 'png')
+    print("All done!")
